33f034d46dfb918855c244f901f84445594adc52
Maria Petrova <petroffama@gmail.com>
2022-09-11 20:11:29 +0300

    fix bug prime

--- src/prime.js
+++ src/prime.js
@@ -9,6 +9,9 @@ const createQuestion = (data) => {
 }
 
 const isPrimeNum = (num) => {
+    if (num < 2) {
+        return false;
+    }
     for (let i = 2; i < num; i++) {
         if (num % i == 0) {
             return false;
